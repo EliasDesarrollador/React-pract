c28a918c3d84f12d0bbde966d774facd587d24de
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-08-29 11:49:27 -0300

    Anatomía de un componente y aplicar estilos con inline stylë́

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+  
+//Agregando estilo a los componentes con inline style
+const estilo = (bg='#333' ) => ({
+  backgroundColor:   bg, 
+  color: '#fff',
+  padding: ' 10px 15px',
+  margin: '10px 15px '
+})
+const Li = ({  children }) => {
   return (
-    <p>Chanchito feliz</p>
+    <li style={estilo ( '#457')} className = 'CLase-Li'>{children}</li>
+  )
+}
 
-  );
+// Anatomía de un componente 
+function Boton () {
+  return (
+    <button>Yo soy un botón</button>
+  )
+} 
+// Con arrow function 
+const BotonArrow = () => {
+  return <button>Yo soy un botón con Arrow Function </button>
+}
+const  App  =   ( ) =>  {
+  const  valor = 'Triste'
+  return (
+    <>
+     <ul style= {estilo('#333')} className = "CLase-css">  
+        <Li estado = 'Feliz'>Valor de LI </Li>
+     </ul>
+     <Boton />
+     <BotonArrow />
+    </>
+  ); 
 }
 
 export default App;
