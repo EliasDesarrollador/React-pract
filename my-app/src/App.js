1f1ba60ed86acb119b5bd007c9b260869cee358c
Elias Ortiz <emails.eliasortiz@gmail.com>
2025-08-28 20:33:02 -0400

    Componentes y propiedades

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
 
-// Fundamentos de React
-// Creación y anidamiento de componentes 
-//  Las aplicaciones React están hechas de componentes. Un componente es una parte de la interfaz de usuario que tiene su propia lógica y apariencia.
-//  Un componente puede ser tan pequeño como un botón o tan grande como una página entera.
-//  Los componentes de React son funciones de JavaScript que devuelven marcado:
-// Ejemplo 1: 
-function MyButton() {
+function App() {
   return (
-      <button> Soy un boton </button>
+    <p>Chanchito feliz</p>
+
   );
 }
 
-// Creación y anidamiento de componentes 
-export default function MyApp(){
-  return (
-      <div>
-          <h1>Bienvenido a mi app</h1>
-          <MyButton/>
-      </div>
-  )
-
-}
+export default App;
