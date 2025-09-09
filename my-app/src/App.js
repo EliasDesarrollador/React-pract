7f14731430fa07a505d15a67c835dc5c93b6134a
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-09 17:20:36 -0300

    Eventosy listas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,23 @@
 
 import Button from "./Button"
+
+const arr = [
+'Chanchito Feliz',
+'Chanchito Feliz',
+'Chanchito Feliz',
+]
 // Reutilizar componentes con estilo 
 const App =  () => { 
+    const miVariable = false
+
+    if (miVariable) {
+        return  <p>Mi variable dio True </p>
+    }
     return (    
      <div>
-        <h1>Hola mundo </h1> 
+        <h1 onClick={( e) => console.log('click', e)}>Hola mundo 
+            </h1> 
+                {arr.map(el => <p key={el}>{el}</p>)}
         <Button onClick = { () => console.log('Clikeado')}> 
             Enviar
         </Button>
