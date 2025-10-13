f0bac42907dc43e15074c368347c52c0e3a32a13
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-13 11:28:56 -0300

    Anatomia de un componente basado en clases

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,41 @@
 
-import Button from "./Button"
+import  {Component} from 'react'
 
-const arr = [
-'Chanchito Feliz',
-'Chanchito Feliz',
-'Chanchito Feliz',
-]
-// Reutilizar componentes con estilo 
-const App =  () => { 
-    const miVariable = false
-
-    if (miVariable) {
-        return  <p>Mi variable dio True </p>
+class App  extends  Component {
+    render() {
+        return (
+         <p>Hola Mundo</p>
+        )
     }
-    return (    
-     <div>
-        <h1 onClick={( e) => console.log('click', e)}>Hola mundo 
-            </h1> 
-                {arr.map(el => <p key={el}>{el}</p>)}
-        <Button onClick = { () => console.log('Clikeado')}> 
-            Enviar
-        </Button>
-
-     </div>
-    )
 }
+
+
 export default App
+
+
+// import Button from "./Button"
+
+// const arr = [
+// 'Chanchito Feliz',
+// 'Chanchito Feliz',
+// 'Chanchito Feliz',
+// ]
+// // Reutilizar componentes con estilo 
+// const App =  () => { 
+//     const miVariable = false
+
+//     if (miVariable) {
+//         return  <p>Mi variable dio True </p>
+//     }
+//     return (    
+//      <div>
+//         <h1 onClick={( e) => console.log('click', e)}>Hola mundo 
+//             </h1> 
+//                 {arr.map(el => <p key={el}>{el}</p>)}
+//         <Button onClick = { () => console.log('Clikeado')}> 
+//             Enviar
+//         </Button>
+
+//      </div>
+//     )
+// }
